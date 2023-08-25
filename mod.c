ab084315b460df11c2564daa5f879e1c4f89f6c5
Lwandiso Pohlwana <85118790+LwandisoPohlwana@users.noreply.github.com>
2023-08-25 14:16:46 +0200

    Update mod.c

--- mod.c
+++ mod.c
@@ -1,42 +1,33 @@
 #include "monty.h"
 /**
- * mod - computes the rest of the division of the second
- * top element of the stack by the top element of the stack
- * @head: stack head
- * @line_num: line number in input file
- * Return: no return
-*/
-
-void mod(stack_t **head, unsigned int line_num)
+ * _mod - mod top of stack y second top stack
+ * @stack: pointer to lists for monty stack
+ * @line_number: number of line opcode occurs on
+ */
+void _mod(stack_t **stack, unsigned int line_number)
 {
-	stack_t *temp_h;
-	int length = 0, tmp;
+	int mod = 0;
 
-	temp_h = *head;
-	while (temp_h)
+	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		temp_h = temp_h->next;
-		length++;
-	}
-	if (length < 2)
-	{
-		fprintf(stderr, "L%d: can't mod, stack too short\n", line_num);
-		fclose(locat.file);
-		free(locat.content);
-		free_stack(*head);
+		fprintf(stderr, "L%u: can't mod, stack too short\n", line_number);
+		free(var_global.buffer);
+		fclose(var_global.file);
+		free_dlistint(*stack);
 		exit(EXIT_FAILURE);
 	}
-	temp_h = *head;
-	if (temp_h->n == 0)
+	else if ((*stack)->n == 0)
 	{
-		fprintf(stderr, "L%d: division by zero\n", line_num);
-		fclose(locat.file);
-		free(locat.content);
-		free_stack(*head);
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		free(var_global.buffer);
+		fclose(var_global.file);
+		free_dlistint(*stack);
 		exit(EXIT_FAILURE);
 	}
-	tmp = temp_h->next->n % temp_h->n;
-	temp_h->next->n = tmp;
-	*head = temp_h->next;
-	free(temp_h);
+	else
+	{
+		mod = (*stack)->n;
+		_pop(stack, line_number);
+		(*stack)->n %= mod;
+	}
 }
