befc3847fdd568096c9b26301b98f5ea384d52d7
Lwandiso Pohlwana <85118790+LwandisoPohlwana@users.noreply.github.com>
2023-08-25 14:14:25 +0200

    Update mul.c

--- mul.c
+++ mul.c
@@ -1,33 +1,25 @@
 #include "monty.h"
 /**
- * mul - multiplies the top two elements of the stack.
- * @head: stack head
- * @line_num: line number in input file
- * Return: no return
-*/
-
-void mul(stack_t **head, unsigned int line_num)
+ * _mul - mul top of stack y second top stack
+ * @stack: pointer to lists for monty stack
+ * @line_number: number of line opcode occurs on
+ */
+void _mul(stack_t **stack, unsigned int line_number)
 {
-	stack_t *temp_h;
-	int length = 0, tmp;
+	int aux;
 
-	temp_h = *head;
-	while (temp_h)
+	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		temp_h = temp_h->next;
-		length++;
-	}
-	if (length < 2)
-	{
-		fprintf(stderr, "L%d: can't mul, stack too short\n", line_num);
-		fclose(locat.file);
-		free(locat.content);
-		free_stack(*head);
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
+		free(var_global.buffer);
+		fclose(var_global.file);
+		free_dlistint(*stack);
 		exit(EXIT_FAILURE);
 	}
-	temp_h = *head;
-	tmp = temp_h->next->n * temp_h->n;
-	temp_h->next->n = tmp;
-	*head = temp_h->next;
-	free(temp_h);
+	else
+	{
+		aux = (*stack)->n;
+		_pop(stack, line_number);
+		(*stack)->n *= aux;
+	}
 }
